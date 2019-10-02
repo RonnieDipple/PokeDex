189a738e6003251f2cb7ff74a9f15ec6e1eabdf8
ronniedipple <ronniedipple@gmail.com>
2019-10-01 22:14:31 -0500

    Initialization

---
+++ src/main/java/com/example/pokedex/Model/PokeDex.kt
@@ -0,0 +1,9 @@
+package com.example.pokedex.Model
+
+class PokeDex {
+    /*
+    public List<Pokemon> pokemon { get; set; }
+     */
+
+    var pokemon: List<Pokemon>?= null //Creates a list from the Pokemon model
+}
